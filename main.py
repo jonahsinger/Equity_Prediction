4c8dae02d6cf74087c60a1b2494334ddc17694ae
Jonah Singer <121359690+jonahsinger@users.noreply.github.com>
2025-05-10 18:29:55 -0400

    plan for data processign

---
+++ main.py
@@ -0,0 +1,13 @@
+# list of 20 large cap that have been around for 25 years
+
+# list of 20 mid cap that have been around for 25 years
+
+# list of 20 small cap that have been around for 25 years
+
+
+# get info from yahoo finance of Features
+#  include historical price change, historical volume change, market capitalization,
+#  price to earnings ratio, and sector classifications.
+
+# put in all one dataframe
+# test to see nan values
