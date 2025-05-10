8af6cb7976edfba9116c8be4f5ade4ed3cdc0284
Sophie <sophiexabla@gmail.com>
2025-05-10 19:13:13 -0400

    20 large, med, small cap

--- main.py
+++ main.py
@@ -4,6 +4,74 @@
 
 # list of 20 small cap that have been around for 25 years
 
+large_cap_tickers = [
+    'AAPL',  
+    'MSFT', 
+    'JNJ',  
+    'PG',    
+    'KO',    
+    'PEP',   
+    'WMT',  
+    'HD',    
+    'XOM',   
+    'CVX',  
+    'JPM',   
+    'BAC',   
+    'WFC',   
+    'MRK',   
+    'PFE',   
+    'INTC',  
+    'CSCO', 
+    'VZ',    
+    'T',    
+    'DIS'    
+]
+
+mid_cap_tickers = [
+    'AAP', 
+    'NUE',   
+    'PKI',  
+    'ROL',   
+    'SJM',   
+    'HRB',   
+    'HSY',   
+    'CLX',  
+    'CPB',  
+    'MKC',   
+    'WSM',   
+    'FLO',  
+    'LEG',   
+    'HAS',   
+    'PII',   
+    'SWK',   
+    'THO',  
+    'MDC',  
+    'TUP',   
+    'SEE'    
+]
+
+small_cap_tickers = [
+    'BGS',   
+    'CALM',  
+    'CATO',  
+    'CBZ',   
+    'CVGW',  
+    'FIZZ',  
+    'HELE', 
+    'LANC',  
+    'MATW',  
+    'PCYG',  
+    'PNM',  
+    'PRGS',  
+    'SLP',  
+    'TILE',  
+    'TREX',  
+    'UEIC',  
+    'UVV',  
+    'WINA', 
+    'WDFC', 
+    'SCX'   
+]
 
 # get info from yahoo finance of Features
 #  include historical price change, historical volume change, market capitalization,
